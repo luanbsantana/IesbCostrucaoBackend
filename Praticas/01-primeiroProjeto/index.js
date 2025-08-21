54a96a91decd8f808fbb9fd2e3a56b934f69d899
luanbsantana <luan.b.santtana@gmail.com>
2025-08-20 21:29:38 -0300

    Criando primeiro projeto Node

---
+++ Praticas/01-primeiroProjeto/index.js
@@ -0,0 +1,14 @@
+console.log("Rodando meu primeiro projeto em Node");
+let nome = "Luan";
+let sobrenome = "Barboza";
+console.log(`${nome} ${sobrenome}`);
+let idade = 28;
+console.log(`A idade do ${nome} ${sobrenome} e ${idade}`);
+
+
+let exercicios = 0.5;
+let trabalho = 1.5;
+let prova = 4.5;
+let notaA1 = exercicios + trabalho + prova;
+
+console.log(`Nota A1: ${notaA1}`);
